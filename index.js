5e58c729e245b04b07cbab17a623828ca2de55eb
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-06-08 22:08:27 +0600

    server progressing

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// const verifyJWT = (req, res, next) => {
-//     const authorization = req.headers.authorization;
-//     if (!authorization) {
-//         return res.status(401).send({ error: true, message: 'unauthorized access' });
-//     }
-//     // bearer token
-//     const token = authorization.split(' ')[1];
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
 
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//         if (err) {
-//             return res.status(401).send({ error: true, message: 'unauthorized access' })
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 // SummerCampSnap
 // I0DQl6yxUo5WBute
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -46,25 +46,43 @@ async function run() {
         const usersCollection = client.db("CampSnapDb").collection("users");
         const classesCollection = client.db("CampSnapDb").collection("classes");
         const instructorsCollection = client.db("CampSnapDb").collection("instructors");
-
-
-
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
             res.send({ token })
         })
-
         // Warning: use verifyJWT before using verifyAdmin
-
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+        const verifyInstructor = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'instructor') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
         /**
            * 0. do not show secure links to those who should not see the links
            * 1. use jwt token: verifyJWT
            * 2. use verifyAdmin middleware
           */
-
         // users related apis
+        app.get('/users', verifyJWT, verifyInstructor, verifyAdmin, async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        });
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
@@ -77,6 +95,57 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+        // security layer: verifyJWT
+        // email same
+        // check admin
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+
+
+        })
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result);
+
+
+        })
+
+        // app.patch('/users/admin/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id);
+        //     const filter = { _id: new ObjectId(id) };
+        //     const updateDoc = {
+        //         $set: {
+        //             role: 'admin'
+        //         },
+        //     };
+
+        //     const result = await usersCollection.updateOne(filter, updateDoc);
+        //     res.send(result);
+
+        // })
+
+
+
+
         // popular classes and instructors related api
         app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray();
